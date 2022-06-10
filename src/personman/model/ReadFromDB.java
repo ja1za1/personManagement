0ecdab9b45cd177b6c9d95523072ae6c389f9510
ja1za1 <jlterrafarias@gmail.com>
2022-06-10 01:59:08 -0300

    removed hardcoded parts

--- src/personman/model/ReadFromDB.java
+++ src/personman/model/ReadFromDB.java
@@ -16,8 +16,16 @@ public class ReadFromDB {
 						 IP = "192.168.0.114",
 						 DATABASEURL = "jdbc:postgresql://"+IP+":5432/peopleManagement",
 						 DATABASEUSER = "read_only",
-						 DATABASEPASSWORD = "garlic2468";
+						 DATABASEPASSWORD = "garlic2468",
+						 SEARCHLOGINQUERY = "SELECT * FROM login";
 	
+	private static final int NAME = 0,
+							 EMAIL = 1,
+							 TELEPHONE = 2,
+							 CPF = 3,
+							 CITY = 4;
+							 
+							 
 	public static List<String> searchUserPassword() {
 		List <String> userPassword = new ArrayList<>();
 		try {
@@ -25,8 +33,7 @@ public class ReadFromDB {
 			Connection conection = DriverManager.getConnection(DATABASEURL,DATABASEUSER,DATABASEPASSWORD);
 			if(conection != null) {
 				Statement statement = conection.createStatement();
-				final String query = "SELECT * from login";
-				ResultSet rs = statement.executeQuery(query); 
+				ResultSet rs = statement.executeQuery(SEARCHLOGINQUERY); 
 				while(rs.next()) {
 					userPassword.add(rs.getString(1));
 					userPassword.add(rs.getString(2));
@@ -39,7 +46,7 @@ public class ReadFromDB {
 	}//List<String> searchUserPassword()
 	
 	public static Person transformPersonData(String[] userData, LocalDate userBirthday) {
-		Person person = new Person(userData[0],userData[1],userData[2],userData[3],userData[4],userBirthday);
+		Person person = new Person(userData[NAME],userData[EMAIL],userData[TELEPHONE],userData[CPF],userData[CITY],userBirthday);
 		return person;
 	}//Person transformPersonData()
 
